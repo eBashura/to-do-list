8bc6a536de3b909ed3c1b642227fe0fa8a660a34
eBashura <126193255+eBashura@users.noreply.github.com>
2023-10-15 13:30:32 +0300

    Update tests_2

--- diplom/src/tests.py
+++ diplom/src/tests.py
@@ -4,21 +4,25 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-class TaskViewTests(TestCase):
+class TaskTests(TestCase):
     def setUp(self):
+        # Create test user
         self.user = User.objects.create_user(username='test_1', password='useruser')
         self.client.login(username='test_1', password='useruser')
 
     def test_task_create_view(self):
+        # Create test task
         response = self.client.post(reverse('task_create'), {'title': 'New Task'})
-        self.assertEqual(response.status_code, 302)  # Check if redirecting
+        self.assertEqual(response.status_code, 302) 
 
     def test_task_update_view(self):
+        # Test task update
         task = Task.objects.create(user=self.user, title='Test Task', description='Description', completed=False)
         response = self.client.post(reverse('task_update', args=[task.pk]), {'title': 'Updated Task'})
-        self.assertEqual(response.status_code, 302)  # Check if redirecting
+        self.assertEqual(response.status_code, 302)  
 
     def test_task_delete_view(self):
+        # Test task delete
         task = Task.objects.create(user=self.user, title='Test Task', description='Description', completed=False)
         response = self.client.post(reverse('task_delete', args=[task.pk]))
         self.assertEqual(response.status_code, 302)
